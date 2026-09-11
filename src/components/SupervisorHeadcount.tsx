b95d72cd4bef6321acd6962f0107095b7e1658ca
suvankar-srtec <suvankar.srtec@gmail.com>
2026-09-11 17:36:39 +0530

    Load supervisor headcount styles

--- src/components/SupervisorHeadcount.tsx
+++ src/components/SupervisorHeadcount.tsx
@@ -2,6 +2,7 @@
 
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { Spinner } from "./LoadingIndicator";
+import "./SupervisorHeadcount.css";
 
 type Headcount = {
   ok: true;
